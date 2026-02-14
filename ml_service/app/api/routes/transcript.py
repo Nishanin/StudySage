1ebd4c4c9e6144d580d1c709bdefced0dbae28a2
pratik0620 <pratikmorkar000@gmail.com>
2026-02-14 19:17:44 +0530

    Added error handing for yt transcript if it is not available

--- ml_service/app/api/routes/transcript.py
+++ ml_service/app/api/routes/transcript.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 
-from scripts.get_transcript import fetch_transcript
+from app.scripts.get_transcript import fetch_transcript
 
 router = APIRouter()
 
